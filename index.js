796f95e530be6e41dcf2a4add10f3042d73c3a46
Antti Harju <git@anttiharju.dev>
2024-10-20 10:09:13 +0300

    Complete 3.2

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ let persons = [
   }
 ]
 
+app.get('/info', (request, response) => {
+  // also show current date and time
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
