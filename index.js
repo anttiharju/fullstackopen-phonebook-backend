50b4f8cd4a6680ad29d89d60985709f9ac5c4b40
Antti Harju <git@anttiharju.dev>
2024-10-20 11:31:04 +0300

    Complete 3.5

--- index.js
+++ index.js
@@ -56,6 +56,39 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? parseInt(Math.random() * Number.MAX_SAFE_INTEGER)
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name is missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number is missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
