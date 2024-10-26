0887a4f98eb98beb79d97ba73eba4f004245c266
Antti Harju <antti@harju.io>
2024-10-26 19:08:32 +0300

    Complete 3.20

--- models/person.js
+++ models/person.js
@@ -23,8 +23,14 @@ const personSchema = new mongoose.Schema({
   },
   number:  {
     type: String,
-    minLength: 2,
-    required: true
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{5,}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
   }
 })
 
