3dd3d363c9e4595361f2b80dc764388c8adc6c7b
Antti Harju <git@anttiharju.dev>
2024-10-26 19:29:33 +0300

    Add stylisticJs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
   js.configs.recommended,
@@ -11,6 +12,27 @@ export default [
         ...globals.node,
       },
       ecmaVersion: "latest",
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
     },
   },
 ]
